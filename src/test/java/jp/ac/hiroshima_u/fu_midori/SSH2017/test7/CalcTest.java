558d3552cfd6d3b8b9904a9cd6a63d02feb8571d
A A <abcdefghijklm@outlook.jp>
2017-05-04 15:02:12 +0900

    jacoco

--- src/test/java/jp/ac/hiroshima_u/fu_midori/SSH2017/test7/CalcTest.java
+++ src/test/java/jp/ac/hiroshima_u/fu_midori/SSH2017/test7/CalcTest.java
@@ -1,15 +1,22 @@
 package jp.ac.hiroshima_u.fu_midori.SSH2017.test7;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by 遠藤拓斗 on 2017/04/24.
  */
 public class CalcTest {
-    @org.junit.Test
+    @Test
     public void add1_2() {
         Calc calc = new Calc();
         assertEquals(3, calc.add(1, 2));
     }
 
+    @Test
+    public void add2_3() {
+        Calc calc = new Calc();
+        assertEquals(5, calc.add(2, 3));
+    }
 }
